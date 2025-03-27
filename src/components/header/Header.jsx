a2e12c6672f9eb558c2447defbde772c8dd99832
Ambrishyadav-bytes <ambrishyadav1110@gmail.com>
2025-03-27 23:08:39 +0530

    updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { use } from 'react'
+import Container from '../container/Container'
+import LogoutBtn from './LogoutBtn'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+const userStatus = useSelector((state) => state.auth.status)
+
+const navigate = useNavigate()
+
+const navItems =[
+  {
+    name : 'Home',
+    slug : '/'
+    active : true
+  },
+  {
+    name : 'login',
+    slug : '/login'
+    active : !userStatus
+  }
+  {
+    name : 'Signup',
+    slug : '/signup'
+    active : !userStatus
+  }
+  {
+    name : 'Profile',
+    slug : '/profile'
+    active : userStatus
+  }
+  {
+    name :'posts',
+    slug : '/posts'
+    active: authStatus
+  }
+  {
+    name :'all posts',
+    slug : '/all-posts'
+    active: authStatus
+  }
+  {
+    name :'create post',
+    slug : '/create-post'
+    active: authStatus
+  }
+  {
+    name :'logout',
+    slug : '/logout'
+    active: authStatus
+  }
+]
 
 function Header() {
   return (
-    <div>header</div>
+    <header className=" py-3 shadow bg-gray-500" >
+      <container>
+        
+      </container>
+    </header>
   )
 }
 
