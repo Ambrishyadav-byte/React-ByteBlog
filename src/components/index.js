8195efda17165ffec3f1320bfb23566707f58913
Ambrishyadav-bytes <ambrishyadav1110@gmail.com>
2025-03-28 23:46:49 +0530

    added more functions

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Feader/Header";
 import Footer from "./Footer/Footer";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import PostCard from "./PostCard";
 
-export { Header, Footer };
+export { Header, Footer, Input , Button, Select, PostCard };
